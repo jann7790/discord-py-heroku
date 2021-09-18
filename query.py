ed8702c5eb175bc6f7ba950e005f5d2e1f433896
jann7790 <jann7790@gmail.com>
2021-09-18 22:43:36 +0800

    que

--- query.py
+++ query.py
@@ -22,8 +22,8 @@ def querySex(arg)->list:
     headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0'}
     params={'limit':100, 'popular':arg}
     r = requests.get('https://www.dcard.tw/service/api/v2/forums/sex/posts', headers=headers, params=params)
+    print(r.text)
     jsons = json.loads(r.text)
-    print(jsons)
     posts = []
     for i in jsons:
         if (i['gender']) == 'F':
@@ -44,6 +44,7 @@ def queryCourse():
     try:
         json={"Semester":"1101","CourseNo":"","CourseName":"","CourseTeacher":"","Dimension":"","CourseNotes":"","ForeignLanguage":0,"OnlyGeneral":1,"OnleyNTUST":0,"OnlyMaster":0,"Language":"zh"}
         r=requests.post('https://querycourse.ntust.edu.tw/querycourse/api/courses', json=json)
+        print(r.text)
         df = pd.read_json(StringIO(r.text))
         df = df.drop(['Semester', 'RequireOption', 'AllYear', 'ThreeStudent', 'AllStudent', 'NTURestrict', 'Restrict2', 'NTNURestrict', 'CourseTimes', 'ClassRoomNo', 'ThreeNode', 'PracticalTimes', 'Dimension', 'CreditPoint', 'Contents'], axis = 1)
         df.loc[:, 'Rate'] =  df.Restrict1 /  df.ChooseStudent
@@ -61,7 +62,6 @@ def queryCourse():
             lastTime = str(lastTime)
             df.to_csv(f'./tmp/{time}_course.csv', index=False, encoding='utf-8')
             df = df[df.CourseNo.apply(lambda x: True if x not in list(last.CourseNo) else False)]
-            print(df)
 
             
             if df.empty:
